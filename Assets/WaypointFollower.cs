50b13cd53b600005b68b33ff418c9aecd0d48bef
awesomlypossom <awesomlypossom@gmail.com>
2023-07-08 00:28:41 -0500

    Made a script for following a waypoint

--- Assets/WaypointFollower.cs
+++ Assets/WaypointFollower.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class WaypointFollower : MonoBehaviour
 {
+    [SerializeField] private GameObject[] waypoints;
+    private int currWaypointIndex = 0;
+    public Vector2 wayPointPos;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        wayPointPos = waypoints[0].transform.position;
+        rb = GetComponent<Rigidbody2D>() ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (wayPointPos != rb.position)
+        {
+            rb.velocity = (wayPointPos - rb.position)*.1f;
+        }
     }
 }
