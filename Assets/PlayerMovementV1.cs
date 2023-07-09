23241ed69e9abb63da168f743d66eb41209e0131
Vicirez <vicramirez829@gmail.com>
2023-07-08 21:37:40 -0700

    Second Pull Request ❤️

--- Assets/PlayerMovementV1.cs
+++ Assets/PlayerMovementV1.cs
@@ -9,42 +9,22 @@ using UnityEngine;
 
 public class PlayerMovementV1 : MonoBehaviour
 {
-    public float dogStrength = 1f;
     public float speedCurr = 3;
     public float maxVel = 10;
     public float walkSpeed = 10;
     public float sprintSpeed = 20;
-    private Rigidbody2D rb;
-    private Vector2 controlVector;
-    [SerializeField] GameObject kidObject;
-    WaypointFollower kidObjVars;
-    private Rigidbody2D kidObjectrb;
-    private Vector2 smoothInputRef;
-    public Vector2 tempVar;
-    public float dampingValue = .2f;
-    public float dogPullingSpeed = 2f;
-    public float doggieDistance;
-
-    void updateSpeed(Vector2 controlVect)
+    void updateSpeed(Vector3 controlVect)
     { 
-        Vector2 currVel = rb.velocity;
-        Vector2 newVel = (controlVect * speedCurr + currVel);
+        Vector3 currVel = GetComponent<Rigidbody2D>().velocity;
+        Vector3 newVel = (controlVect * speedCurr + currVel);
         if(newVel.magnitude < maxVel)
         {
-            rb.velocity = Vector2.SmoothDamp(currVel,newVel, ref smoothInputRef,dampingValue);
+            GetComponent<Rigidbody2D>().velocity = newVel;
         }
     }
-    Vector2 getControls()
-    {    
-        if(Input.GetKey("space"))
-        {
-            maxVel = sprintSpeed;
-        }
-        else
-        {
-            maxVel = walkSpeed;
-        }
-        Vector2 controlVect = new Vector2(0,0);
+    Vector3 getControls()
+    {
+        Vector3 controlVect = new Vector3(0,0,0);
         if(Input.GetKey("w"))
         {
             controlVect[1] = controlVect[1] + 1;
@@ -66,45 +46,18 @@ public class PlayerMovementV1 : MonoBehaviour
         }
         return controlVect;
     }
-    void leashInfluance()
-    {
-        if(Vector2.Distance(kidObjectrb.position,rb.position) >= kidObjVars.leashLength)
-        { 
-            if(Vector2.Distance(kidObjectrb.position,rb.position) >= kidObjVars.leashLength*1.5)
-            {
-                tempVar = (kidObjectrb.position - rb.position).normalized;
-                controlVector = (controlVector + tempVar*2).normalized;
-            }
-            else
-            {
-                tempVar = (kidObjectrb.position - rb.position).normalized;
-                controlVector = (controlVector + tempVar).normalized;
-                //maxVel = dogPullingSpeed;
-            }
-            
-        }
 
-    }
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>() ;
-        kidObjectrb = kidObject.GetComponent<Rigidbody2D>();
-        kidObjVars = kidObject.GetComponent<WaypointFollower>();
+        Debug.Log("helloWorld");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        controlVector = getControls();
-        leashInfluance();
-        updateSpeed(controlVector);
-        doggieDistance = Vector2.Distance(kidObjectrb.position,rb.position);
-        
-        
-        
-        
-        
+        updateSpeed(getControls());
         if(Input.GetKey("space"))
         {
             maxVel = sprintSpeed;
--- Assets/WaypointFollower.cs
+++ Assets/WaypointFollower.cs
@@ -4,78 +4,15 @@ using UnityEngine;
 
 public class WaypointFollower : MonoBehaviour
 {
-
-    public float childSpeed = 2;
-    public Vector2 totalVelocity = new Vector2(0,0);
-    public float dogStreangth = 1f;
-    [SerializeField] GameObject[] waypoints;
-    [SerializeField] GameObject dogObject;
-    public int currWaypointIndex = 0;
-    public Vector2 wayPointPos;
-    public float leashLength = 3.0f;
-    private Rigidbody2D rb;
-
-    void waypointInfluence()
-    {
-        if (Vector2.Distance(wayPointPos, rb.position) >= .1)
-        {
-            totalVelocity = totalVelocity + ((wayPointPos - rb.position).normalized*childSpeed);
-        }
-        else
-        {
-            currWaypointIndex++;
-            if(currWaypointIndex >= waypoints.GetLength(0))
-            {
-                currWaypointIndex = 0;
-            }
-            wayPointPos =  waypoints[currWaypointIndex].transform.position;
-        }
-    }
-
-    void updateposition()
-    {
-        rb.velocity = totalVelocity;
-    }
-
-    void dogInflence()
-    {
-        Vector2 dogPosition = new Vector2(dogObject.transform.position.x,dogObject.transform.position.y);
-        if (Vector2.Distance(dogPosition,rb.position)  >=leashLength ) 
-        {
-            totalVelocity = totalVelocity + ((dogPosition - rb.position).normalized) * dogStreangth;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
-        wayPointPos = waypoints[0].transform.position;
-        rb = GetComponent<Rigidbody2D>() ;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        totalVelocity.x = 0;
-        totalVelocity.y = 0;
-        waypointInfluence();
-        dogInflence();
-
-
-
-
-        updateposition();
         
     }
 }
